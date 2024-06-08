6aaf476242e807b04dc5dcb74063706a4043bc1c
VladyslavKos <kostucikvladislav@gmail.com>
2024-06-08 19:19:47 +0200

    Finished proxy configuration

--- src/main/java/Configuration/Configuration.java
+++ src/main/java/Configuration/Configuration.java
@@ -4,19 +4,28 @@ import io.cucumber.java.After;
 import io.cucumber.java.AfterAll;
 import io.cucumber.java.BeforeAll;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Configuration {
     private WebDriver driver;
+
     public WebDriver getDriver() {
         if (driver == null) {
-            ChromeOptions options = new ChromeOptions();
-            String proxy = "127.0.0.1:8080"; // Proxy server and port
-            options.addArguments("--proxy-server=http://" + proxy);
 //            System.setProperty("webdriver.chrome.driver", "src/main/resources/Drivers/chromedriver");
+
+//            Proxy proxy = new Proxy();
+//            proxy.setAutodetect(false);
+//            proxy.setHttpProxy("127.0.0.1:8080");
+//            proxy.setSslProxy("127.0.0.1:8080");
+//            ChromeOptions chromeOptions = new ChromeOptions();
+//            chromeOptions.setCapability("proxy", proxy);
+//            chromeOptions.setAcceptInsecureCerts(true);
+
             WebDriverManager.chromedriver().setup();
+//            driver = new ChromeDriver(chromeOptions);
             driver = new ChromeDriver();
         }
         return driver;
--- src/main/java/Configuration/ProxyConfig.java
+++
@@ -1,5 +0,0 @@
-package Configuration;
-
-public class ProxyConfig {
-
-}
